bedfd7e0f98842cb5ccad2b99732eceb1c6e4042
Fernando <junio132sj@gmail.com>
2023-12-24 15:09:43 -0300

    fix

--- public/js/default.js
+++ public/js/default.js
@@ -1,8 +1,8 @@
 document.getElementById('button-discord').addEventListener('click',()=>{
     console.log('discord');
-    fetch('http://localhost:3000/test/discord',{method:'POST'})
+    fetch('http://192.168.0.101:3000/test/discord',{method:'POST'})
 })
 document.getElementById('button-chrome').addEventListener('click',()=>{
     console.log('chrome');
-    fetch('http://localhost:3000/test/chrome',{method:'POST'})
+    fetch('http://192.168.0.101:3000/test/chrome',{method:'POST'})
 })
