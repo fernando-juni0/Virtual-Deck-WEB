f5185c8caa06d7dea287bc33e9436ad00de849b4
Fernando <junio132sj@gmail.com>
2023-12-24 18:16:35 -0300

    fix

--- public/js/default.js
+++ public/js/default.js
@@ -1,7 +1,7 @@
 init()
 async function init() {
     let ip = await fetch('https://api.ipify.org/?format=json').then(response => response.json()).then(data => {return data.ip}).catch(error => {console.error('Erro ao obter o IP:', error);});
-    const socket = new WebSocket(`ws://${ip}:8080`);
+    const socket = new WebSocket(`wss://${ip}:8080`);
     
     document.getElementById('button-discord').addEventListener('click',()=>{
         if (socket.readyState === WebSocket.OPEN) {
