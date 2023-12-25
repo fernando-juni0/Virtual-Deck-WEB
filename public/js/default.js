82e50d484c5b0f5a26a9cf155d268df3ee58683d
Fernando <junio132sj@gmail.com>
2023-12-25 14:47:33 -0300

    fix

--- public/js/default.js
+++ public/js/default.js
@@ -5,7 +5,7 @@ document.getElementById('button-discord').addEventListener('click', async()=>{
     let ip = await fetch('https://api.ipify.org/?format=json').then(response => response.json()).then(data => {return data}).catch(error => {console.error('Erro ao obter o IP:', error);});
     console.log(ip);
     try {
-        const response = await fetch('http://192.168.1.101:3000/executar-funcao', {
+        const response = await fetch('https://192.168.1.101:3000/executar-funcao', {
         method: 'GET' // Pode ser outro método HTTP dependendo da necessidade
         });
         const data = await response.text();
