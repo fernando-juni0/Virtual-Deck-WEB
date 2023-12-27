043e2065821ecf294ae180dfeee6e0a001785d9b
Fernando <junio132sj@gmail.com>
2023-12-27 16:18:59 -0300

    fix

--- public/js/default.js
+++ public/js/default.js
@@ -4,11 +4,11 @@
 document.getElementById('button-discord').addEventListener('click', async()=>{
     let ip = await fetch('https://api.ipify.org/?format=json').then(response => response.json()).then(data => {return data.ip}).catch(error => {console.error('Erro ao obter o IP:', error);});
     try {
-        const response = await fetch(`https://a244-177-11-159-231.ngrok-free.app/executar-funcao`, {
-        method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
-        headers: {
-            'ngrok-skip-browser-warning': '1'
-        }
+        const response = await fetch(`https://c63e-177-11-159-231.ngrok-free.app/executar-funcao`, {
+            method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
+            headers: {
+                'ngrok-skip-browser-warning': '1'
+            }
         });
         const data = await response.text();
         console.log(data); // Mensagem da resposta do aplicativo desktop
