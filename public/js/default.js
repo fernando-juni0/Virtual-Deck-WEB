1160496ed2cec434a628a3c015c250fa77f8d6e0
Fernando <junio132sj@gmail.com>
2023-12-29 18:41:01 -0300

    fix

--- public/js/default.js
+++ public/js/default.js
@@ -1,8 +1,11 @@
 let linkServer
-let param = location.pathname.replace('/default/','')
+const match = location.href.match(/\/default\/(\d+)?$/);
+const param = match ? (match[1] || '') : '';
+console.log(param);
 if (param.length > 0) {
     linkServer = param
     document.getElementById('linkServer').value = param
+    document.getElementById('config-popup').style.display = 'none'
 }
 
 
@@ -15,28 +18,115 @@ document.getElementById('close-config-popup-button').addEventListener('click',()
 })
 
 
-document.getElementById('save-linkServer').addEventListener('click',()=>{
+document.getElementById('save-linkServer').addEventListener('click',async()=>{
     let link = document.getElementById('linkServer').value
     if (link.trim().length > 0) {
-        linkServer = link
-    }
-})
-
-document.getElementById('button-discord').addEventListener('click', async()=>{
-    if (linkServer) {
         try {
-            const response = await fetch(`https://${linkServer}/executar-funcao`, {
+            const response = await fetch(`https://${linkServer}/ping`, {
                 method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
                 headers: {
                     'ngrok-skip-browser-warning': '1'
                 }
             });
             const data = await response.text();
+            if (data == 'sucess') {
+                successNotify('Você se conectou a um dispositivo!')
+            }else{
+                errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto!")
+            }
             console.log(data); // Mensagem da resposta do aplicativo desktop
         } catch (error) {
             console.error('Erro:', error);
+            errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto!")
+        }
+        linkServer = link
+        document.getElementById('config-popup').style.display = 'none'
+        location.pathname = '/default/'+link
+
+
+    }
+})
+
+document.getElementById('button-discord').addEventListener('click', async()=>{
+    if (linkServer) {
+        try {
+            const response = await fetch(`https://${linkServer}/discord`, {
+                method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
+                headers: {
+                    'ngrok-skip-browser-warning': '1'
+                }
+            });
+            const data = await response.text();
+
+            console.log(data); // Mensagem da resposta do aplicativo desktop
+        } catch (error) {
+            console.error('Erro:', error);
+            errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto ou atualize a página!")
         }
     }
 })
+
+
+document.getElementById('button-chrome').addEventListener('click', async()=>{
+    if (linkServer) {
+        try {
+            const response = await fetch(`https://${linkServer}/chrome`, {
+                method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
+                headers: {
+                    'ngrok-skip-browser-warning': '1'
+                }
+            });
+            const data = await response.text();
+
+            console.log(data); // Mensagem da resposta do aplicativo desktop
+        } catch (error) {
+            console.error('Erro:', error);
+            errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto ou atualize a página!")
+        }
+    }
+})
+
+
+document.getElementById('button-twitch').addEventListener('click', async()=>{
+    if (linkServer) {
+        try {
+            const response = await fetch(`https://${linkServer}/twitch`, {
+                method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
+                headers: {
+                    'ngrok-skip-browser-warning': '1'
+                }
+            });
+            const data = await response.text();
+
+            console.log(data); // Mensagem da resposta do aplicativo desktop
+        } catch (error) {
+            console.error('Erro:', error);
+            errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto ou atualize a página!")
+        }
+    }
+})
+
+
+document.getElementById('button-spotify').addEventListener('click', async()=>{
+    if (linkServer) {
+        try {
+            const response = await fetch(`https://${linkServer}/spotify`, {
+                method: 'GET', // Pode ser outro método HTTP dependendo da necessidade
+                headers: {
+                    'ngrok-skip-browser-warning': '1'
+                }
+            });
+            const data = await response.text();
+
+            console.log(data); // Mensagem da resposta do aplicativo desktop
+        } catch (error) {
+            console.error('Erro:', error);
+            errorNotify("Erro ao se comunicar com o dispositivo, verifique se o código de acesso está correto ou atualize a página!")
+        }
+    }
+})
+
+
+
 
 
